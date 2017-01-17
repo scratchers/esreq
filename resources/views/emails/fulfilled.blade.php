417ab8a7ae6553a31caf9d1cd7184ebb57ecec32
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-17 16:44:49 +0000

    fix well-formed html email view

--- resources/views/emails/fulfilled.blade.php
+++ resources/views/emails/fulfilled.blade.php
@@ -1,21 +1,29 @@
-<a href="{{ url('/home/'.$esrequest->id) }}">Your request for access</a> to Enterprise Systems from the department of
-Information Systems in the Sam M. Walton College of Business at
-the University of Arkansas in Fayetteville, Arkansas has been fulfilled.
+<!doctype html><html><body>
 
-<br/>
-<br/>
+<p>
+    <a href="{{ url('/home/'.$esrequest->id) }}">Your request for access</a>
+    to Enterprise Systems
+    from the department of Information Systems
+    in the Sam M. Walton College of Business
+    at the University of Arkansas
+    in Fayetteville, Arkansas has been fulfilled.
+</p>
 
-<table border=1>
-    <tbody>
-        @foreach($esrequest->getFields() as $key => $value)
-            <tr>
-                <td>{{  $key  }}</td>
-                <td>{{ $value }}</td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+<p>
+    <table border=1>
+        <tbody>
+            @foreach($esrequest->getFields() as $key => $value)
+                <tr>
+                    <td>{{  $key  }}</td>
+                    <td>{{ $value }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</p>
 
-<br/>
+<p>
+    {{ $esrequest->note }}
+</p>
 
-{{ $esrequest->note }}
+</body></html>
