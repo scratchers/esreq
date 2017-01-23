bcedc19b8a69d72400bf036781946f499791d046
Jeff Puckett <jeff@jeffpuckett.com>
2017-01-23 15:33:55 +0000

    set custom route on home controller for show view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,6 +27,9 @@ class HomeController extends Controller
     public function index()
     {
         $esrequests = Auth::user()->esrequests()->get();
+
+        $esrequests->controller = 'HomeController@show';
+
         return view('esrequests.index', compact('esrequests'));
     }
 
