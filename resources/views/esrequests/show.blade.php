91beb3295b9dfce7ce14f7f5c9dfc7044fd5a90b
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-10 22:53:56 +0000

    remove confusing breadcrumb

--- resources/views/esrequests/show.blade.php
+++ resources/views/esrequests/show.blade.php
@@ -1,12 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <ol class="breadcrumb">
-        <li><a href="/">Welcome</a></li>
-        <li><a href='{{ route("$route.index") }}'>Requests</a></li>
-        <li class="active">{{ $esrequest->id }}</li>
-    </ol>
-
     <h1>Request Details</h1>
 
     @include('esrequests.partials.esrequest')
