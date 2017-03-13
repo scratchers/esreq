2c493d8c08c99c02e70f6d4da3f6508d59148b53
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-13 21:30:20 +0000

    add date range picker

--- resources/views/report/index.blade.php
+++ resources/views/report/index.blade.php
@@ -5,12 +5,15 @@
 
     @include('report.partials.breadcrumb')
 
-    <div style="padding:5px">
-        <a href="?csv=1" class="btn btn-success">
-            <i class="fa fa-table" aria-hidden="true"></i>
-            Download CSV
-        </a>
-    </div>
+<div>
+    <form>
+        <label for="date-from">Date From</label>
+        <input type="text" id="date-from" name="date-from">
+        <label for="date-to">to</label>
+        <input type="text" id="date-to" name="date-to">
+        <button type="submit" class="btn btn-default">Filter by Date Range</button>
+    </form>
+</div>
 
     <table class="datatable datatable-report">
         <thead>
@@ -50,6 +53,13 @@
         </tbody>
     </table>
 
+<div style="padding:5px">
+    <a href="?csv=1" class="btn btn-success">
+        <i class="fa fa-table" aria-hidden="true"></i>
+        Download CSV
+    </a>
+</div>
+
 <script>
 $(document).ready(function() {
     $('.datatable-report').DataTable({
@@ -75,6 +85,41 @@ $(document).ready(function() {
             });
         }
     });
+
+    var dateFormat = "yy-mm-dd",
+        from = $( "#date-from" )
+            .datepicker({
+                dateFormat: "yy-mm-dd",
+                defaultDate: "+1w",
+                changeMonth: true,
+                changeYear: true,
+                numberOfMonths: 3
+            })
+            .on( "change", function() {
+                to.datepicker( "option", "minDate", getDate( this ) );
+            }),
+        to = $( "#date-to" ).datepicker({
+                dateFormat: "yy-mm-dd",
+                defaultDate: "+1w",
+                changeMonth: true,
+                changeYear: true,
+                numberOfMonths: 3
+            })
+            .on( "change", function() {
+                from.datepicker( "option", "maxDate", getDate( this ) );
+            });
+
+    function getDate( element ) {
+        var date;
+
+        try {
+            date = $.datepicker.parseDate( dateFormat, element.value );
+        } catch( error ) {
+            date = null;
+        }
+
+        return date;
+    }
 });
 </script>
 @endsection
