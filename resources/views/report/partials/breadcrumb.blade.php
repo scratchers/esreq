3d611c301d40cb44937fdbca615be5b470b1a32d
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-14 18:39:34 +0000

    fix query string on report breadcrumb

--- resources/views/report/partials/breadcrumb.blade.php
+++ resources/views/report/partials/breadcrumb.blade.php
@@ -3,7 +3,7 @@
     @unless ( empty($breadcrumbs) )
     @foreach ( $breadcrumbs as $breadcrumb )
         @unless ( empty($breadcrumb['link']) )
-            <li><a href="{{ $breadcrumb['link'] }}">{{ $breadcrumb['text'] }}</a></li>
+            <li><a href="{{ $breadcrumb['link'] }}?{{ $query }}">{{ $breadcrumb['text'] }}</a></li>
         @else
             <li class="active">{{ $breadcrumb['text'] }}</li>
         @endunless
