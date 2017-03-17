338eaaacdeb03ff3337ca36cb2c89964e00f4c34
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-17 19:28:55 +0000

    add migration for faculty account data

--- database/migrations/2017_03_16_174501_migrate_esrequest_platform_data.php
+++ database/migrations/2017_03_16_174501_migrate_esrequest_platform_data.php
@@ -11,6 +11,7 @@ class MigrateEsrequestPlatformData extends Migration
      */
     public function up()
     {
+        // this *depends* on 2017_03_10_174501_seed_platforms_table.php
         $platforms = [
             1 => 'IBM',
             2 => 'Microsoft',
