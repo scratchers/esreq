bf141afab53286f92cce4471a0ac175c6b77fa56
Jeff Puckett <jeff@jeffpuckett.com>
2017-03-21 17:12:40 +0000

    use datatable for faculty account index

--- resources/views/facultyAccounts/index.blade.php
+++ resources/views/facultyAccounts/index.blade.php
@@ -1,26 +1,44 @@
 @extends('layouts.app')
 
+<?php $platforms = App\Platform::all(); ?>
+
 @section('content')
 <h1>Faculty Accounts</h1>
 
-<ul>
-    @forelse ( $facultyAccounts as $facultyAccount )
-    <li>
-        <a
-            href="{{ route('customer.facultyAccount.show', $facultyAccount) }}"
-            onclick="return showAjaxModal(this)"
-        >
-            {{ $facultyAccount->username }}
-        </a>
-
-        @foreach ( $facultyAccount->platforms as $platform )
-        <label class="label label-{{ $platform->name }}">
-            {{ $platform->name }}
-        </label>
-        @endforeach
-    </li>
-    @empty
+@if ( $facultyAccounts->isEmpty() )
     <p class="lead">You have no faculty accounts.</p>
-    @endforelse
-</ul>
+@else
+<table class="datatable">
+    <thead>
+        <tr>
+            <th>User</th>
+            <th>Username</th>
+            @foreach ( $platforms as $platform )
+                <th>{{ $platform->name }}</th>
+            @endforeach
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ( $facultyAccounts as $facultyAccount )
+        <tr>
+            <td>{{ $facultyAccount->user->name }}</td>
+
+            <td>
+                <a
+                    href="{{ route('customer.facultyAccount.show', $facultyAccount) }}"
+                    onclick="return showAjaxModal(this)"
+                >
+                    {{ $facultyAccount->username }}
+                </a>
+            </td>
+
+            @foreach ( $platforms as $platform )
+                <td>{{ $facultyAccount->platforms->contains($platform) ? $platform->name : '' }}</td>
+            @endforeach
+        </tr>
+        @endforeach
+    </tbody>
+</table>
+@endif
+
 @endsection
