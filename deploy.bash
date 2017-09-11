d06afb953528cfd1c70ed0c04f1a93da25dced60
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-11 16:14:19 +0000

    fix deploy script
    explicit clear-compiled
    migrate after cache

--- deploy.bash
+++ deploy.bash
@@ -2,14 +2,20 @@
 
 set -e
 
-composer install --no-dev
-
 php artisan down
-php artisan key:generate
-php artisan migrate
+
+php artisan clear-compiled
+composer install --no-dev
 php artisan optimize
+
+php artisan key:generate
+
 php artisan cache:clear
 php artisan route:cache
 php artisan config:cache
+
+php artisan migrate
+
 php artisan up
+
 php artisan opcache:clear
