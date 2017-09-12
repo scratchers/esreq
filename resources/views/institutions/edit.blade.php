0637a246d53e3bbd26e1ae40b531165374f4e8c9
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-12 19:09:35 +0000

    add save button for institution location

--- resources/views/institutions/edit.blade.php
+++ resources/views/institutions/edit.blade.php
@@ -5,37 +5,77 @@
     <p><a href="{{ $institution->url }}">{{ $institution->url }}</a></p>
 
     @unless (empty($institution->latitude))
-    <p>Location: {{ $institution->latitude}}, {{$institution->longitude }}</p>
+    <p>Location: {{ $institution->latitude }}, {{ $institution->longitude }}</p>
     @endunless
 
-    <div id="map" style="height:800px"></div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <p>
+                Drag and drop the position marker
+                to show the institution's location.
+                Then click the save button.
+            </p>
+            <div id="map" style="height:800px"></div>
+        </div>
+        <div class="panel-footer">
+            <button onclick="saveLocation()" class="btn btn-primary">
+                Save
+            </button>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
     <script>
-    var map;
+    var map, marker;
 
     function initMap()
     {
-        var wcob = {lat: 36.06463197792664, lng: -94.17427137166595};
+        var location = {lat: {{ $institution->latitude or 36.06463197792664 }}, lng: {{ $institution->longitude or -94.17427137166595 }}};
+
         map = new google.maps.Map(document.getElementById('map'), {
-            center: wcob,
-            zoom: 13
+            center: location,
+            zoom: 13,
         });
 
-        var marker = new google.maps.Marker({
-            position: wcob,
+        marker = new google.maps.Marker({
+            position: location,
             map: map,
             draggable: true,
-            title: "Drag me!"
+            title: "Drag me!",
         });
+    }
 
-        google.maps.event.addListener(marker, 'dragend', function (evt) {
-            console.log('lat: ' + evt.latLng.lat() + ' lng: ' + evt.latLng.lng());
+    function saveLocation()
+    {
+        $.ajax({
+            type: "PATCH",
+            url: "{{ route('institutions.update', $institution) }}",
+            data: {
+                "_token": "{{ csrf_token() }}",
+                "latitude": marker.position.lat(),
+                "longitude": marker.position.lng(),
+            },
+            success: function (data) {
+                $('#saveLocationConfirmation').modal();
+            }
         });
     }
     </script>
     <script async defer
         src="https://maps.googleapis.com/maps/api/js?key={{ config('google-maps.key') }}&callback=initMap">
     </script>
+
+    <div class="modal fade" id="saveLocationConfirmation" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-body">
+              The institution has been updated successfully!
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+          </div>
+        </div>
+      </div>
+    </div>
 @endpush
