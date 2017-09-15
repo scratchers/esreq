a1734e0ec0737a8bdae3cfb0e1d62024b21084b1
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-15 14:50:26 +0000

    add zttp test mocks

--- app/GoogleMaps.php
+++ app/GoogleMaps.php
@@ -2,7 +2,36 @@
 
 namespace App;
 
+use Zttp\PendingZttpRequest;
+use Zttp\ZttpResponse;
+
 class GoogleMaps
 {
-    
+    protected $zttp;
+
+    public function __construct(PendingZttpRequest $zttp)
+    {
+        $this->zttp = $zttp;
+    }
+
+    public function queryLatLng(string $name) : array
+    {
+        $response = $this->get(['address' => $name]);
+
+        return $this->extractLatLng($response);
+    }
+
+    public function get(array $options = []) : ZttpResponse
+    {
+        return $this->zttp->get('https://maps.googleapis.com/maps/api/geocode/json',
+            array_merge($options, [
+                'key' => config('google-maps.key')
+            ])
+        );
+    }
+
+    protected function extractLatLng(ZttpResponse $response) : array
+    {
+        return $response->json()['results'][0]['geometry']['location'] ?? [];
+    }
 }
--- tests/Unit/GoogleMapsQueryTest.php
+++ tests/Unit/GoogleMapsQueryTest.php
@@ -4,11 +4,35 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\GoogleMaps;
+use Zttp\PendingZttpRequest;
+use Zttp\ZttpResponse;
 
 class GoogleMapsQueryTest extends TestCase
 {
     public function test_instantiates_class()
     {
-        $this->assertInstanceOf(GoogleMaps::class, new GoogleMaps);
+        $zttp = new PendingZttpRequest;
+        $this->assertInstanceOf(GoogleMaps::class, new GoogleMaps($zttp));
+    }
+
+    public function test_returns_mock()
+    {
+        $expected = [
+            'lat' => 36.06463197792664,
+            'lng' => -94.17427137166595,
+        ];
+
+        $stubResponse = $this->createMock(ZttpResponse::class);
+        $array['results'][0]['geometry']['location'] = $expected;
+        $stubResponse->method('json')
+            ->willReturn($array);
+
+        $stubZttp = $this->createMock(PendingZttpRequest::class);
+        $stubZttp->method('get')
+            ->willReturn($stubResponse);
+
+        $gmaps = new GoogleMaps($stubZttp);
+
+        $this->assertSame($expected, $gmaps->queryLatLng('someplace'));
     }
 }
