e8a39cbe523fdcf98f5e7c8c53301862f41e880b
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-18 20:32:50 +0000

    test user can register with existing institution

--- tests/Browser/RegistrationTest.php
+++ tests/Browser/RegistrationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Institution;
 
 class RegistrationTest extends DuskTestCase
 {
@@ -43,7 +44,6 @@ class RegistrationTest extends DuskTestCase
             $asu = '"name":"Arkansas State University - Jonesboro","url":"https:\/\/www.astate.edu\/","latitude":35.843086,"longitude":-95.06939';
             $browser
                 ->visit('/register')
-                ->assertSee('Register')
                 ->type('institution', 'Arkansas State University - Jonesboro')
                 ->press('Register')
                 ->type('url', 'https://www.astate.edu/')
@@ -55,8 +55,34 @@ class RegistrationTest extends DuskTestCase
                 ->type('password', 'password')
                 ->type('password_confirmation', 'password')
                 ->press('Register')
+                ->assertPathIs('/home')
                 ->visit('/institutions?json')
                 ->assertSee($asu)
+                ->visit('/shibboleth-logout')
+            ;
+        });
+    }
+
+    public function test_user_can_register_with_existing_institution()
+    {
+        $this->browse(function (Browser $browser) {
+            $institution = create(Institution::class);
+
+            $browser
+                ->visit('/register')
+                ->type('institution', substr($institution->name, 0, -1))
+                ->waitFor('#ui-id-1')
+                ->click('#ui-id-1 li:first-child')
+                ->press('Register')
+                ->assertPathIs('/register')
+                ->type('first_name', 'John')
+                ->type('last_name', 'Doe')
+                ->type('email', 'johndoe@example.com')
+                ->type('password', 'password')
+                ->type('password_confirmation', 'password')
+                ->press('Register')
+                ->assertPathIs('/home')
+                ->visit('/shibboleth-logout')
             ;
         });
     }
