0bcad128316d203bea764599480d37c1791d22ac
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-18 19:49:53 +0000

    test drag and drop marker

--- tests/Browser/RegistrationTest.php
+++ tests/Browser/RegistrationTest.php
@@ -10,11 +10,6 @@ class RegistrationTest extends DuskTestCase
 {
     use DatabaseMigrations;
 
-    /**
-     * A basic browser test example.
-     *
-     * @return void
-     */
     public function test_user_can_register()
     {
         $this->browse(function (Browser $browser) {
@@ -35,6 +30,31 @@ class RegistrationTest extends DuskTestCase
                 ->type('password_confirmation', 'password')
                 ->press('Register')
                 ->assertPathIs('/home')
+                ->visit('/institutions?json')
+                ->assertSee($asu)
+                ->visit('/shibboleth-logout')
+            ;
+        });
+    }
+
+    public function test_user_can_register_drag_drop_location()
+    {
+        $this->browse(function (Browser $browser) {
+            $asu = '"name":"Arkansas State University - Jonesboro","url":"https:\/\/www.astate.edu\/","latitude":35.843086,"longitude":-95.06939';
+            $browser
+                ->visit('/register')
+                ->assertSee('Register')
+                ->type('institution', 'Arkansas State University - Jonesboro')
+                ->press('Register')
+                ->type('url', 'https://www.astate.edu/')
+                ->dragLeft('#map img', 100)
+                ->press('Submit')
+                ->type('first_name', 'John')
+                ->type('last_name', 'Doe')
+                ->type('email', 'johndoe@example.com')
+                ->type('password', 'password')
+                ->type('password_confirmation', 'password')
+                ->press('Register')
                 ->visit('/institutions?json')
                 ->assertSee($asu)
             ;
