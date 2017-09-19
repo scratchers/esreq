e842311772c30d5760ee89519853242227fab1f2
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-19 20:53:00 +0000

    fix exception reporting in test

--- app/Exceptions/Reporter.php
+++ app/Exceptions/Reporter.php
@@ -4,11 +4,16 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Auth\AuthenticationException;
+use App;
 
 class Reporter
 {
     public static function wants(Exception $exception) : bool
     {
+        if (App::environment() === 'testing') {
+            return false;
+        }
+
         if ( $exception instanceof AuthenticationException ) {
             return false;
         }
