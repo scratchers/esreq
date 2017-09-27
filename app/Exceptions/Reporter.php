6f6806826161f2c7ba79d8ac060ce2a54043b446
Jeff Puckett <jeff@jeffpuckett.com>
2017-09-27 13:58:29 +0000

    add config to ignore specific 404s

--- app/Exceptions/Reporter.php
+++ app/Exceptions/Reporter.php
@@ -5,6 +5,7 @@ namespace App\Exceptions;
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use App;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Reporter
 {
@@ -16,6 +17,15 @@ class Reporter
 
         if ( $exception instanceof AuthenticationException ) {
             return false;
+        }
+
+        if ( $exception instanceof HttpException ) {
+            $key = 'exception-handler.'.HttpException::class.'.ignore-routes';
+            $ignored = collect(config($key, []));
+
+            if ($ignored->contains(request()->path())) {
+                return false;
+            }
         }
 
         return true;
